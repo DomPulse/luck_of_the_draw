98a534cdea5fba78720f7875eb81006ffca8b985
Dominic Polsinelli <32404918+DomPulse@users.noreply.github.com>
2025-10-08 01:16:28 -0500

    Add files via upload

--- ps2_basic.ino
+++ ps2_basic.ino
@@ -35,7 +35,7 @@ void loop ( )
       data_byte = (ps2Read[1] << 7) + (ps2Read[2] << 6) + (ps2Read[3] << 5) + (ps2Read[4] << 4) + (ps2Read[5] << 3) + (ps2Read[6] << 2) + (ps2Read[7] << 1) + (ps2Read[8] << 0);
       Serial.print(gaming);
       Serial.print(" ");
-      Serial.println(data_byte);
+      Serial.write(data_byte);
 
     }
   }
@@ -46,93 +46,97 @@ void convert_char(int data)
 {
   if (shifted)
   {
-    if (data == 56) Serial.println("A");
-    else if (data == 76) Serial.println("B");
-    else if (data == 132) Serial.println("C");
-    else if (data == 196) Serial.println("D");
-    else if (data == 36) Serial.println("E");
-    else if (data == 212) Serial.println("F");
-    else if (data == 44) Serial.println("G");
-    else if (data == 204) Serial.println("H");
-    else if (data == 194) Serial.println("I");
-    else if (data == 220) Serial.println("J");
-    else if (data == 66) Serial.println("K");
-    else if (data == 210) Serial.println("L");
-    else if (data == 92) Serial.println("M");
-    else if (data == 140) Serial.println("N");
-    else if (data == 34) Serial.println("O");
-    else if (data == 178) Serial.println("P");
-    else if (data == 168) Serial.println("Q");
-    else if (data == 180) Serial.println("R");
-    else if (data == 216) Serial.println("S");
-    else if (data == 52) Serial.println("T");
-    else if (data == 60) Serial.println("U");
-    else if (data == 84) Serial.println("V");
-    else if (data == 184) Serial.println("W");
-    else if (data == 68) Serial.println("X");
-    else if (data == 172) Serial.println("Y");
-    else if (data == 88) Serial.println("Z"); 
-    else if (data == 148) Serial.println(" "); 
-    else if (data == 104) Serial.println("!");
-    else if (data == 120) Serial.println("@");
-    else if (data == 100) Serial.println("#");
-    else if (data == 164) Serial.println("$");
-    else if (data == 116) Serial.println("%"); 
-    else if (data == 108) Serial.println("^");
-    else if (data == 188) Serial.println("&");
-    else if (data == 124) Serial.println("*");
-    else if (data == 98) Serial.println("("); 
-    else if (data == 162) Serial.println(")"); 
-    else if (data == 130) Serial.println("<"); 
-    else if (data == 146) Serial.println(">"); 
-    else if (data == 82) Serial.println("?"); 
-    else if (data == 74) Serial.println('"'); 
+    if (data == 56) Serial.write("A");
+    else if (data == 76) Serial.write("B");
+    else if (data == 132) Serial.write("C");
+    else if (data == 196) Serial.write("D");
+    else if (data == 36) Serial.write("E");
+    else if (data == 212) Serial.write("F");
+    else if (data == 44) Serial.write("G");
+    else if (data == 204) Serial.write("H");
+    else if (data == 194) Serial.write("I");
+    else if (data == 220) Serial.write("J");
+    else if (data == 66) Serial.write("K");
+    else if (data == 210) Serial.write("L");
+    else if (data == 92) Serial.write("M");
+    else if (data == 140) Serial.write("N");
+    else if (data == 34) Serial.write("O");
+    else if (data == 178) Serial.write("P");
+    else if (data == 168) Serial.write("Q");
+    else if (data == 180) Serial.write("R");
+    else if (data == 216) Serial.write("S");
+    else if (data == 52) Serial.write("T");
+    else if (data == 60) Serial.write("U");
+    else if (data == 84) Serial.write("V");
+    else if (data == 184) Serial.write("W");
+    else if (data == 68) Serial.write("X");
+    else if (data == 172) Serial.write("Y");
+    else if (data == 88) Serial.write("Z"); 
+    else if (data == 148) Serial.write(" "); 
+    else if (data == 104) Serial.write("!");
+    else if (data == 120) Serial.write("@");
+    else if (data == 100) Serial.write("#");
+    else if (data == 164) Serial.write("$");
+    else if (data == 116) Serial.write("%"); 
+    else if (data == 108) Serial.write("^");
+    else if (data == 188) Serial.write("&");
+    else if (data == 124) Serial.write("*");
+    else if (data == 98) Serial.write("("); 
+    else if (data == 162) Serial.write(")"); 
+    else if (data == 130) Serial.write("<"); 
+    else if (data == 146) Serial.write(">"); 
+    else if (data == 82) Serial.write("?"); 
+    else if (data == 74) Serial.write('"'); 
+    else if (data == 102) Serial.write(0xC);
+    else if (data == 90) Serial.write("\n");
     else Serial.println(data); 
   }
   
   else
   {
-    if (data == 56) Serial.println("a");
-    else if (data == 76) Serial.println("b");
-    else if (data == 132) Serial.println("c");
-    else if (data == 196) Serial.println("d");
-    else if (data == 36) Serial.println("e");
-    else if (data == 212) Serial.println("f");
-    else if (data == 44) Serial.println("g");
-    else if (data == 204) Serial.println("h");
-    else if (data == 194) Serial.println("i");
-    else if (data == 220) Serial.println("j");
-    else if (data == 66) Serial.println("k");
-    else if (data == 210) Serial.println("l");
-    else if (data == 92) Serial.println("m");
-    else if (data == 140) Serial.println("n");
-    else if (data == 34) Serial.println("o");
-    else if (data == 178) Serial.println("p");
-    else if (data == 168) Serial.println("q");
-    else if (data == 180) Serial.println("r");
-    else if (data == 216) Serial.println("s");
-    else if (data == 52) Serial.println("t");
-    else if (data == 60) Serial.println("u");
-    else if (data == 84) Serial.println("v");
-    else if (data == 184) Serial.println("w");
-    else if (data == 68) Serial.println("x");
-    else if (data == 172) Serial.println("y");
-    else if (data == 88) Serial.println("z"); 
-    else if (data == 148) Serial.println(" "); 
-    else if (data == 104) Serial.println("1");
-    else if (data == 120) Serial.println("2");
-    else if (data == 100) Serial.println("3");
-    else if (data == 164) Serial.println("4");
-    else if (data == 116) Serial.println("5"); 
-    else if (data == 108) Serial.println("6");
-    else if (data == 188) Serial.println("7");
-    else if (data == 124) Serial.println("8");
-    else if (data == 98) Serial.println("9"); 
-    else if (data == 162) Serial.println("0"); 
-    else if (data == 130) Serial.println(","); 
-    else if (data == 146) Serial.println("."); 
-    else if (data == 82) Serial.println("/"); 
-    else if (data == 74) Serial.println("'"); 
+    if (data == 56) Serial.write("a");
+    else if (data == 76) Serial.write("b");
+    else if (data == 132) Serial.write("c");
+    else if (data == 196) Serial.write("d");
+    else if (data == 36) Serial.write("e");
+    else if (data == 212) Serial.write("f");
+    else if (data == 44) Serial.write("g");
+    else if (data == 204) Serial.write("h");
+    else if (data == 194) Serial.write("i");
+    else if (data == 220) Serial.write("j");
+    else if (data == 66) Serial.write("k");
+    else if (data == 210) Serial.write("l");
+    else if (data == 92) Serial.write("m");
+    else if (data == 140) Serial.write("n");
+    else if (data == 34) Serial.write("o");
+    else if (data == 178) Serial.write("p");
+    else if (data == 168) Serial.write("q");
+    else if (data == 180) Serial.write("r");
+    else if (data == 216) Serial.write("s");
+    else if (data == 52) Serial.write("t");
+    else if (data == 60) Serial.write("u");
+    else if (data == 84) Serial.write("v");
+    else if (data == 184) Serial.write("w");
+    else if (data == 68) Serial.write("x");
+    else if (data == 172) Serial.write("y");
+    else if (data == 88) Serial.write("z"); 
+    else if (data == 148) Serial.write(" "); 
+    else if (data == 104) Serial.write("1");
+    else if (data == 120) Serial.write("2");
+    else if (data == 100) Serial.write("3");
+    else if (data == 164) Serial.write("4");
+    else if (data == 116) Serial.write("5"); 
+    else if (data == 108) Serial.write("6");
+    else if (data == 188) Serial.write("7");
+    else if (data == 124) Serial.write("8");
+    else if (data == 98) Serial.write("9"); 
+    else if (data == 162) Serial.write("0"); 
+    else if (data == 130) Serial.write(","); 
+    else if (data == 146) Serial.write("."); 
+    else if (data == 82) Serial.write("/"); 
+    else if (data == 74) Serial.write("'");
+    else if (data == 102) Serial.write(0xC);
+    else if (data == 90) Serial.write("\n");
     else Serial.println(data); 
   }
   //90 -> enter
@@ -155,7 +159,7 @@ void incr()
 {
   ps2Read[gaming] = digitalRead(3);
   gaming++;
-  //Serial.println(gaming);
+  //Serial.write(gaming);
   if (gaming == 11)
   {
     detachInterrupt(digitalPinToInterrupt (2));
@@ -164,7 +168,7 @@ void incr()
     data_byte = (ps2Read[1] << 7) + (ps2Read[2] << 6) + (ps2Read[3] << 5) + (ps2Read[4] << 4) + (ps2Read[5] << 3) + (ps2Read[6] << 2) + (ps2Read[7] << 1) + (ps2Read[8] << 0);
     if (ps2Read[0] == 0 & ps2Read[10] == 1)
     {
-      //Serial.println(data_byte); 
+      //Serial.write(data_byte); 
       if (data_byte == 72 | data_byte == 154)
       {
         shifted = !ready;
